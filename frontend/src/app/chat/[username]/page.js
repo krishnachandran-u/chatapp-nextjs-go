8d23dc292b810b5688ad202d1cb53c99cabd1889
krishnachandran-u <kriss52450@gmail.com>
2024-03-02 23:21:46 +0530

    create the message buffer div

--- frontend/src/app/chat/[username]/page.js
+++ frontend/src/app/chat/[username]/page.js
@@ -2,6 +2,7 @@ const Chat = ({params}) => {
     return (
         <div className = "bg-white w-screen h-screen text-black flex flex-col justify-between items-center p-[24px] gap-[16px]">
            <div className = "w-full bg-slate-100 overflow-y-scroll h-full rounded-[16px]">
+                
             </div> 
            <div className = "bg-white flex flex-col justify-center items-start w-full gap-[16px]">
             <input className = "border-[1px] border-1 border-slate-300 rounded-[4px] p-[8px] w-[200px]" type = "text" placeholder = "sender" />
