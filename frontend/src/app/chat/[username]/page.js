61e2d6dd093a4a027644373b642c2d97cf942362
krishnachandran-u <kriss52450@gmail.com>
2024-03-02 22:43:01 +0530

    user page  finish

--- frontend/src/app/chat/[username]/page.js
+++ frontend/src/app/chat/[username]/page.js
@@ -1,9 +1,14 @@
 const Chat = ({params}) => {
     return (
-        <div className = "bg-white w-screen h-screen text-black flex flex-col justify-between items-center">
-           <div className = "w-full bg-slate-100"></div> 
-           <div className = "bg-white flex flex-col justify-center items-start w-full">
+        <div className = "bg-white w-screen h-screen text-black flex flex-col justify-between items-center p-[24px] gap-[16px]">
+           <div className = "w-full bg-slate-100 overflow-y-scroll h-full rounded-[16px]">
+            </div> 
+           <div className = "bg-white flex flex-col justify-center items-start w-full gap-[16px]">
             <input className = "border-[1px] border-1 border-slate-300 rounded-[4px] p-[8px] w-[200px]" type = "text" placeholder = "sender" />
+            <div className = "w-full flex flex-row gap-[10px]">
+                <input className = "border-[1px] border-1 border-slate-300 rounded-[4px] p-[8px] w-full" type = "text" placeholder = "message" />
+                <button className = "bg-black text-white rounded-[8px] p-[8px] hover:bg-slate-500 transition-colors duration-300">send</button>
+            </div>
            </div>
         </div>
     )
