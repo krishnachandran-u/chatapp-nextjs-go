4ddf45a532c179ebc2e539d65a744f63759a450f
krishnachandran-u <kriss52450@gmail.com>
2024-03-04 12:49:45 +0530

    add some animations

--- frontend/src/app/chat/[username]/page.js
+++ frontend/src/app/chat/[username]/page.js
@@ -1,6 +1,7 @@
 "use client"
 import { useEffect, useState, useRef, useLayoutEffect } from "react";
 import { IoMdSend } from "react-icons/io";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Chat = ({params}) => {
     const inputRef = useRef("");
@@ -56,14 +57,22 @@ const Chat = ({params}) => {
 
     return (
         <div className = "bg-white w-screen h-screen text-black flex flex-col justify-between items-center p-[24px] gap-[16px] lg:p-[96px] md:text-2xl md:p-[32px]">
-           <div className = "w-full bg-slate-100 overflow-y-scroll h-full rounded-[16px] p-[16px] flex flex-col gap-[16px] transiton-all duration-300" ref = {chatContainerRef}>
+           <div className = "w-full bg-slate-100 overflow-y-scroll h-full rounded-[16px] p-[16px] flex flex-col gap-[16px] transiton-all duration-300 overflow-x-hidden" ref = {chatContainerRef}>
+            <AnimatePresence>
                 {messages.map((message, index) => (
-                    <div className = {`bg-slate-300 rounded-[8px] flex flex-col gap-[8px] md:p-[16px] p-[8px] ${message.sender_id == params.username? "items-end lg:ml-[200px] md:ml-[100px] ml-[50px]" : "lg:mr-[200px] md:mr-[100px] mr-[50px]"}`}>
+                    <motion.div 
+                        key = {index}
+                        initial = {{ opacity: 0, y: 20 }}
+                        animate = {{ opacity: 1, y: 0 }}
+                        exit = {{ opacity: 0, y: -20 }}
+                        className = {`bg-slate-300 rounded-[8px] flex flex-col gap-[8px] md:p-[16px] p-[8px] overflow-hidden ${message.sender_id == params.username? "items-end lg:ml-[200px] md:ml-[100px] ml-[50px]" : "lg:mr-[200px] md:mr-[100px] mr-[50px]"}`}
+                    >
                         <div className = "md:text-base font-bold">{/*Sender:*/} {message.sender_id}</div>
                         {/*<div className = "md:text-base">Receiver: {message.receiver_id}</div>*/}
                         <div className = "md:text-2xl text-xl"> {/*Message:*/} {message.content} </div>
-                    </div>
+                    </motion.div>
                 ))}
+            </AnimatePresence>
             </div> 
            <div className = "bg-white flex flex-col justify-center items-start w-full gap-[16px]">
             <input className = "border-[1px] border-1 border-slate-300 rounded-[4px] p-[8px] md:w-[500px] w-[200px]" type = "text" placeholder = "receiver" ref = {receiverRef} />
